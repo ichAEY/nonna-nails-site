d30b7ccc626eeba2bae1728d6e1331ec301546e7
Mac <melikyan.mac@gmail.com>
2026-08-10 16:07:10 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   images: { unoptimized: true },
+  basePath: "/nonna-nails-site",
+  assetPrefix: "/nonna-nails-site/",
 };
 
 export default nextConfig;
