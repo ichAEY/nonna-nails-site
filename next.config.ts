a187798828ca4361803e3d7421e55cfe9a4e1b14
Mac <melikyan.mac@gmail.com>
2026-08-11 13:17:17 +0300

    Save ClayTone version 1.0

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  images: { unoptimized: true },
-  basePath: "/nonna-nails-site",
-  assetPrefix: "/nonna-nails-site/",
+  /* config options here */
 };
 
 export default nextConfig;
